df958d99820949e8067fd0e40cae1a4fb6ae0182
Matt Pazaryna <mpazaryna@gmail.com>
2020-04-03 14:15:23 -0400

    additional function with a test

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # sls-hills
+
+```bash
+sls invoke test
+```
+
+```bash
+sls create function -f myFunction --handler functions/myFunction/index.handler --path {function}
+```
+
+```bash
+serverless invoke local -f calc
+```
+
+## Links
+
+https://github.com/nordcloud/serverless-jest-plugin
---
+++ src/calc001/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports.handler = (event, context, callback) => {
+  var dat = {
+    name: "Joe",
+    zipCode: "02214",
+    income: 110000,
+    maxMonthlyPayment: 1200,
+    mortgageTotal: 230000,
+    totalPriceHouse: 250000,
+    totalInterest: 23000,
+  };
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: { message: dat },
+    }),
+  };
+  callback(null, response);
+};
