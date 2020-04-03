450f2f291aaa61a92d3cbb10852222a9905643be
Matt Pazaryna <mpazaryna@gmail.com>
2020-04-03 15:33:34 -0400

    Slow and steady

--- src/calc001/index.js
+++ src/calc001/index.js
@@ -1,15 +1,19 @@
 "use strict";
 
+var total = 3 + 2;
+
+var dat = {
+  name: "Joe Blow",
+  zipCode: "02214",
+  income: 110000,
+  maxMonthlyPayment: 1200,
+  mortgageTotal: 230000,
+  totalPriceHouse: 250000,
+  totalInterest: 23000,
+  totalCalc: total,
+};
+
 module.exports.handler = (event, context, callback) => {
-  var dat = {
-    name: "Joe",
-    zipCode: "02214",
-    income: 110000,
-    maxMonthlyPayment: 1200,
-    mortgageTotal: 230000,
-    totalPriceHouse: 250000,
-    totalInterest: 23000,
-  };
   const response = {
     statusCode: 200,
     body: JSON.stringify({ message: dat }),
