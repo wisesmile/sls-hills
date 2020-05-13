d7bae569d2efb475caa5c6d8df6ff437f6d4284f
Matt Pazaryna <mpazaryna@gmail.com>
2020-05-13 13:49:20 -0400

    copied in another example

--- functions/middyTest/handler.js
+++ functions/middyTest/handler.js
@@ -1,29 +1,66 @@
-// import core
-const middy = require("@middy/core");
-// import some middlewares
-const jsonBodyParser = require("@middy/http-json-body-parser");
-const httpErrorHandler = require("@middy/http-error-handler");
+const middy = require("middy");
+const {
+  jsonBodyParser,
+  validator,
+  httpErrorHandler,
+} = require("middy/middlewares");
 
-const processPayment = (event, context, callback) => {
-  const {
-    creditCardNumber,
-    expiryMonth,
-    expiryYear,
-    cvc,
-    nameOnCard,
-    amount,
-  } = event.body;
-
-  // do stuff with this data
+/* Normal lambda code */
+const businessLogic = (event, context, callback) => {
+  // event.body has already been turned into an object by `jsonBodyParser` middleware
+  const { name } = event.body;
+  console.log("name", name);
   return callback(null, {
-    result: "success",
-    message: "payment processed correctly",
+    statusCode: 200,
+    body: JSON.stringify({
+      result: "success",
+      message: `Hi ${name} ⊂◉‿◉つ`,
+    }),
   });
 };
 
-// Let's "middyfy" our handler, then we will be able to attach middlewares to it
-const handler = middy(processPayment)
-  .use(jsonBodyParser()) // parses the request body when it's a JSON and converts it to an object
-  .use(httpErrorHandler()); // handles common http errors and returns proper responses
+/* Input & Output Schema */
+const schema = {
+  inputSchema: {
+    type: "object",
+    properties: {
+      body: {
+        type: "object",
+        required: ["name"],
+        properties: {
+          name: { type: "string" },
+        },
+      },
+    },
+    required: ["body"],
+  },
+  outputSchema: {
+    type: "object",
+    properties: {
+      body: {
+        type: "object",
+        required: ["result", "message"],
+        properties: {
+          result: { type: "string" },
+          message: { type: "string" },
+        },
+      },
+    },
+    required: ["body"],
+  },
+};
 
-module.exports = { handler };
+/* Attach middlewares */
+const handler = middy(businessLogic)
+  // parses the request body when it's a JSON and converts it to an object
+  .use(jsonBodyParser())
+  // validates the input
+  .use(validator(schema))
+  // handles common http errors and returns proper responses
+  .use(httpErrorHandler());
+
+/* Export handler for serverless to use */
+module.exports.middyFunction = handler;
+
+/* Export inputSchema & outputSchema for automatic documentation */
+module.exports.schema = schema;
