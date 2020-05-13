f28b05324441d69e10104cb8693c56c286eee414
Matt Pazaryna <mpazaryna@gmail.com>
2020-05-13 14:07:50 -0400

    still hacking

--- functions/middyTest/handler.js
+++ functions/middyTest/handler.js
@@ -1,13 +1,8 @@
-const middy = require("middy");
-const {
-  jsonBodyParser,
-  validator,
-  httpErrorHandler,
-} = require("middy/middlewares");
+const middy = require("@middy/core");
+const jsonBodyParser = require("@middy/http-json-body-parser");
+const httpErrorHandler = require("@middy/http-error-handler");
 
-/* Normal lambda code */
 const businessLogic = (event, context, callback) => {
-  // event.body has already been turned into an object by `jsonBodyParser` middleware
   const { name } = event.body;
   console.log("name", name);
   return callback(null, {
@@ -19,48 +14,9 @@ const businessLogic = (event, context, callback) => {
   });
 };
 
-/* Input & Output Schema */
-const schema = {
-  inputSchema: {
-    type: "object",
-    properties: {
-      body: {
-        type: "object",
-        required: ["name"],
-        properties: {
-          name: { type: "string" },
-        },
-      },
-    },
-    required: ["body"],
-  },
-  outputSchema: {
-    type: "object",
-    properties: {
-      body: {
-        type: "object",
-        required: ["result", "message"],
-        properties: {
-          result: { type: "string" },
-          message: { type: "string" },
-        },
-      },
-    },
-    required: ["body"],
-  },
-};
-
-/* Attach middlewares */
+// Let's "middyfy" our handler, then we will be able to attach middlewares to it
 const handler = middy(businessLogic)
-  // parses the request body when it's a JSON and converts it to an object
-  .use(jsonBodyParser())
-  // validates the input
-  .use(validator(schema))
-  // handles common http errors and returns proper responses
-  .use(httpErrorHandler());
+  .use(jsonBodyParser()) // parses the request body when it's a JSON and converts it to an object
+  .use(httpErrorHandler()); // handles common http errors and returns proper responses
 
-/* Export handler for serverless to use */
-module.exports.middyFunction = handler;
-
-/* Export inputSchema & outputSchema for automatic documentation */
-module.exports.schema = schema;
+module.exports = { handler };
