eba1df3b8b54473df1f88f45fd32c328095c41e4
Matt Pazaryna <mpazaryna@gmail.com>
2020-05-13 12:48:35 -0400

    removed # at top of file

--- functions/middyTest/handler.js
+++ functions/middyTest/handler.js
@@ -1,47 +1,60 @@
-# handler.js
-
 // import core
-const middy = require('@middy/core')
+const middy = require("@middy/core");
 
 // import some middlewares
-const jsonBodyParser = require('@middy/http-json-body-parser')
-const httpErrorHandler = require('@middy/http-error-handler')
-const validator = require('@middy/validator')
+const jsonBodyParser = require("@middy/http-json-body-parser");
+const httpErrorHandler = require("@middy/http-error-handler");
+const validator = require("@middy/validator");
 
 // This is your common handler, in no way different than what you are used to doing every day in AWS Lambda
 const processPayment = (event, context, callback) => {
- // we don't need to deserialize the body ourself as a middleware will be used to do that
- const { creditCardNumber, expiryMonth, expiryYear, cvc, nameOnCard, amount } = event.body
+  // we don't need to deserialize the body ourself as a middleware will be used to do that
+  const {
+    creditCardNumber,
+    expiryMonth,
+    expiryYear,
+    cvc,
+    nameOnCard,
+    amount,
+  } = event.body;
 
- // do stuff with this data
- return callback(null, { result: 'success', message: 'payment processed correctly'})
-}
+  // do stuff with this data
+  return callback(null, {
+    result: "success",
+    message: "payment processed correctly",
+  });
+};
 
 // Notice that in the handler you only added base business logic (no deserilization,
 // validation or error handler), we will add the rest with middlewares
 
 const inputSchema = {
- type: 'object',
- properties: {
-   body: {
-     type: 'object',
-     properties: {
-       creditCardNumber: { type: 'string', minLength: 12, maxLength: 19, pattern: '\d+' },
-       expiryMonth: { type: 'integer', minimum: 1, maximum: 12 },
-       expiryYear: { type: 'integer', minimum: 2017, maximum: 2027 },
-       cvc: { type: 'string', minLength: 3, maxLength: 4, pattern: '\d+' },
-       nameOnCard: { type: 'string' },
-       amount: { type: 'number' }
-     },
-     required: ['creditCardNumber'] // Insert here all required event properties
-   }
- }
-}
+  type: "object",
+  properties: {
+    body: {
+      type: "object",
+      properties: {
+        creditCardNumber: {
+          type: "string",
+          minLength: 12,
+          maxLength: 19,
+          pattern: "d+",
+        },
+        expiryMonth: { type: "integer", minimum: 1, maximum: 12 },
+        expiryYear: { type: "integer", minimum: 2017, maximum: 2027 },
+        cvc: { type: "string", minLength: 3, maxLength: 4, pattern: "d+" },
+        nameOnCard: { type: "string" },
+        amount: { type: "number" },
+      },
+      required: ["creditCardNumber"], // Insert here all required event properties
+    },
+  },
+};
 
 // Let's "middyfy" our handler, then we will be able to attach middlewares to it
 const handler = middy(processPayment)
   .use(jsonBodyParser()) // parses the request body when it's a JSON and converts it to an object
-  .use(validator({inputSchema})) // validates the input
-  .use(httpErrorHandler()) // handles common http errors and returns proper responses
+  .use(validator({ inputSchema })) // validates the input
+  .use(httpErrorHandler()); // handles common http errors and returns proper responses
 
-module.exports = { handler }
+module.exports = { handler };
