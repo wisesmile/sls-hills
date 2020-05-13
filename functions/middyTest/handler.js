c59ad525f79c585c3ae644694e3f3d1ce4ca35d9
Matt Pazaryna <mpazaryna@gmail.com>
2020-05-13 15:41:33 -0400

    adding validator

--- functions/middyTest/handler.js
+++ functions/middyTest/handler.js
@@ -1,6 +1,7 @@
 const middy = require("@middy/core");
 const jsonBodyParser = require("@middy/http-json-body-parser");
 const httpErrorHandler = require("@middy/http-error-handler");
+const validator = require("@middy/validator");
 
 const businessLogic = (event, context, callback) => {
   const { name } = event.body;
@@ -14,9 +15,23 @@ const businessLogic = (event, context, callback) => {
   });
 };
 
+const inputSchema = {
+  type: "object",
+  properties: {
+    body: {
+      type: "object",
+      properties: {
+        name: { type: "string" },
+      },
+      required: ["name"], // Insert here all required event properties
+    },
+  },
+};
+
 // Let's "middyfy" our handler, then we will be able to attach middlewares to it
 const handler = middy(businessLogic)
   .use(jsonBodyParser()) // parses the request body when it's a JSON and converts it to an object
+  .use(validator({ inputSchema })) // validates the input
   .use(httpErrorHandler()); // handles common http errors and returns proper responses
 
 module.exports = { handler };
