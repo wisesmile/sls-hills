1df1892a510edd91d0d2a65bb8ab2f3b4f145b8f
Matt Pazaryna <mpazaryna@gmail.com>
2020-05-13 15:46:41 -0400

    validator is not working as expected

--- functions/middyTest/handler.js
+++ functions/middyTest/handler.js
@@ -31,7 +31,6 @@ const inputSchema = {
 // Let's "middyfy" our handler, then we will be able to attach middlewares to it
 const handler = middy(businessLogic)
   .use(jsonBodyParser()) // parses the request body when it's a JSON and converts it to an object
-  .use(validator({ inputSchema })) // validates the input
   .use(httpErrorHandler()); // handles common http errors and returns proper responses
 
 module.exports = { handler };
