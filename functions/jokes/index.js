4eeb896b92db380297276bc957667469f445460d
Matt Pazaryna <mpazaryna@gmail.com>
2020-05-13 09:59:36 -0400

    slight changes after a failed post

--- functions/jokes/index.js
+++ functions/jokes/index.js
@@ -2,26 +2,27 @@
 
 const { getDadJoke } = require("./utils");
 
-module.exports.handler = (event, context, callback) => {
+module.exports.calc = async (event, context) => {
   if (event.body) {
     event = JSON.parse(event.body);
   }
 
-  let i = event.firstName;
-  let v = getDadJoke();
+  let n = event.name;
+  console.log(`param ==> ${n}`);
 
-  var responseObject = {
-    name: i,
-    message: "Hello, World",
-    dadJoke: v,
+  // execute the adder function
+  let c = getDadJoke();
+  console.log(`joke => ${j}`);
+
+  // create an object to hold all the values
+  var obj = {
+    name: event.name,
+    name_two: n,
+    joke: c,
   };
 
   return {
     statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-    },
-    body: JSON.stringify(responseObject),
+    body: JSON.stringify({ message: obj }),
   };
 };
