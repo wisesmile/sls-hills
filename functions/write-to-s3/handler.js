c802434bd8bc9b1f033ff1e5218d75b75ea9b834
mpaz <mpazaryna@gmail.com>
2020-08-20 12:27:08 -0400

    my bad, probably

--- functions/write-to-s3/handler.js
+++ functions/write-to-s3/handler.js
@@ -1,31 +1,15 @@
 "use strict";
 
-const fetch = require("node-fetch");
 const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
-
 const s3 = new AWS.S3();
 
 module.exports.save = (event, context, callback) => {
-  fetch(event.image_url)
-    .then((response) => {
-      if (response.ok) {
-        return response;
-      }
-      return Promise.reject(
-        new Error(
-          `Failed to fetch ${response.url}: ${response.status} ${response.statusText}`
-        )
-      );
-    })
-    .then((response) => response.buffer())
-    .then((buffer) =>
-      s3
-        .putObject({
-          Bucket: process.env.BUCKET,
-          Key: event.key,
-          Body: buffer,
-        })
-        .promise()
-    )
-    .then((v) => callback(null, v), callback);
+  var content = "This is a sample text file";
+  var keyName = "test.txt";
+  var params = { Bucket: process.env.BUCKET, Key: keyName, Body: content };
+
+  s3.putObject(params, function (err, data) {
+    if (err) console.log(err);
+    else console.log("Successfully saved object.");
+  });
 };
